5a8398cd7c7b7848de739a5077f846823c76b60c
Rinku Sharma <rsharma3@xavient.com>
2016-08-10 15:41:41 +0530

    intial commit

---
+++ src/main/java/com/spring/ws/data/first/SpringWsDataFirstApplication.java
@@ -0,0 +1,12 @@
+package com.spring.ws.data.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWsDataFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWsDataFirstApplication.class, args);
+	}
+}
